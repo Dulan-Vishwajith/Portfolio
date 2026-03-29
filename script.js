4651c9300ddfc4d6b86ccdf2ebd7f502181df3ab
Dulan Vishwajith <dulanvishwajithgunarathna@gmail.com>
2026-03-30 00:16:20 +0530

    90% completed one

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//--Header Section Navigating--//
+
 //function for scroll when click name that in top right corner
 document.getElementById("goOverview").addEventListener("click", function () {
 document.getElementById("overview").scrollIntoView({
@@ -18,3 +20,59 @@ function gotToEducation(){
                 behavior:"smooth"
         });
 }
+//function for scroll when click project button
+function goToProject(){
+        document.getElementById("project").scrollIntoView({
+                behavior: "smooth"
+        });
+}
+
+//function for scroll when click publication button
+function goToPublications(){
+        document.getElementById("publications").scrollIntoView({
+                behavior:"smooth"
+        });
+}
+
+//function for scroll when click contact button
+function goToContact(){
+        document.getElementById("contact").scrollIntoView({
+                behavior:"smooth"
+        });
+}       
+
+
+//--Project Section--Dropdown//
+const cards = document.querySelectorAll(".project-card");
+
+cards.forEach(card => {
+card.addEventListener("click", () => {
+
+        // Close others (optional)
+        cards.forEach(c => {
+        if (c !== card) c.classList.remove("active");
+        });
+
+        // Toggle clicked one
+        card.classList.toggle("active");
+});
+});
+
+
+
+
+//--Publication Section--Dropdown//
+const pub_cards = document.querySelectorAll(".publication-card");
+
+pub_cards.forEach(card =>{
+card.addEventListener("click", () => {
+
+        // Close others (optional)
+        pub_cards.forEach(c => {
+        if (c !== card) c.classList.remove("active");
+        });    
+        
+        // Toggle clicked one
+        card.classList.toggle("active");
+})
+});
